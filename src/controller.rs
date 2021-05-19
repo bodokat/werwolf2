2fa9dd80be531bbf4892c731b59988422f6596fe
bodokat <bod.kato@gmail.com>
2021-05-19 21:26:32 +0200

    add slash command support

--- src/controller.rs
+++ src/controller.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
 
-use itertools::Itertools;
 use serenity::{
     client::{Context, EventHandler},
     model::{
         channel::{ChannelType, Message, Reaction},
         id::{GuildId, UserId},
+        interactions::{Interaction, InteractionResponseType},
+        prelude::User,
     },
 };
 
@@ -13,8 +14,10 @@ use tokio::sync::RwLock;
 
 use async_trait::async_trait;
 
-use crate::lobby::{Lobby, LobbyMessage};
-use crate::PREFIX;
+use crate::{
+    lobby::{Lobby, LobbyMessage},
+    PREFIX,
+};
 
 #[derive(Debug)]
 pub enum ReactionAction {
@@ -64,6 +67,83 @@ impl EventHandler for Controller {
             }
         }
     }
+
+    async fn interaction_create(&self, ctx: Context, mut interaction: Interaction) {
+        if let Some(x) = &interaction.data {
+            match x.name.as_str() {
+                "ping" => interaction
+                    .create_interaction_response(ctx, |r| {
+                        r.kind(InteractionResponseType::ChannelMessageWithSource);
+                        r.interaction_response_data(|m| m.content("Pong"))
+                    })
+                    .await
+                    .expect("Error sending message"),
+                "join" => {
+                    interaction
+                        .create_interaction_response(&ctx, |r| {
+                            r.kind(InteractionResponseType::DeferredChannelMessageWithSource)
+                        })
+                        .await
+                        .expect("Error sending interaction response");
+                    let guild_id = match interaction.guild_id {
+                        Some(id) => id,
+                        None => {
+                            interaction
+                                .edit_original_interaction_response(&ctx, |m| {
+                                    m.content("Du kannst nur aus Server-Kanälen joinen")
+                                })
+                                .await
+                                .expect("Error sending interaction response");
+                            return;
+                        }
+                    };
+                    let user = interaction.member.take().unwrap().user;
+                    let user_name = user.name.clone();
+                    self.join(guild_id, user, &ctx).await;
+                    interaction
+                        .edit_original_interaction_response(&ctx, |m| {
+                            m.content(format!("Willkommen in der Lobby, {}", user_name,))
+                        })
+                        .await
+                        .expect("Error sending interaction response");
+                }
+                "joinall" => {
+                    interaction
+                        .create_interaction_response(&ctx, |r| {
+                            r.kind(InteractionResponseType::DeferredChannelMessageWithSource)
+                        })
+                        .await
+                        .expect("Error sending interaction response");
+                    let guild_id = match interaction.guild_id {
+                        Some(id) => id,
+                        None => {
+                            interaction
+                                .edit_original_interaction_response(&ctx, |m| {
+                                    m.content("Du kannst nur aus Server-Kanälen joinen")
+                                })
+                                .await
+                                .expect("Error sending interaction response");
+                            return;
+                        }
+                    };
+                    self.join_all(guild_id, interaction.member.unwrap().user, &ctx)
+                        .await;
+                }
+                "leave" => {
+                    let user = match interaction.member {
+                        Some(x) => x.user,
+                        None => interaction.user.unwrap(),
+                    };
+                    self.leave(user).await;
+                }
+                "start" => {
+                    self.start(interaction.guild_id.unwrap()).await;
+                }
+                _ => (),
+            }
+        }
+    }
+
     async fn message(&self, ctx: Context, message: Message) {
         if !message.content.starts_with(PREFIX) {
             return;
@@ -90,117 +170,110 @@ impl EventHandler for Controller {
                         return;
                     }
                 };
-                let mut guard = self.lobbies.write().await;
-                let lobby = guard.entry(guild_id).or_insert_with(|| {
-                    println!("Creating new lobby");
-                    Lobby::new(ctx.clone())
-                });
-                let author_id = message.author.id;
-                let res = lobby.0.send(LobbyMessage::Join(message.author)).await;
-                if let Err(tokio::sync::mpsc::error::SendError(LobbyMessage::Join(author))) = res {
-                    // Lobby is closed, create a new one
-                    println!("Creating new lobby (was closed)");
-                    *lobby = Lobby::new(ctx.clone());
-                    let _ = lobby.0.send(LobbyMessage::Join(author)).await;
-                }
-
-                self.messengers
-                    .write()
-                    .await
-                    .insert(author_id, lobby.clone());
+                self.join(guild_id, message.author, &ctx).await;
             }
-            "add" => {
+            "joinall" => {
                 let guild_id = match message.guild_id {
                     Some(x) => x,
                     None => return,
                 };
-
-                let channels = match ctx.http.get_channels(guild_id.0).await {
-                    Ok(x) => x,
-                    Err(e) => {
-                        println!("error getting channel: {}", e);
-                        return;
-                    }
-                };
-
-                let mut to_add = None;
-                let ctx = &ctx;
-                for channel in channels.iter().filter(|c| c.kind == ChannelType::Voice) {
-                    let members = match channel.members(ctx).await {
-                        Ok(x) => x,
-                        Err(e) => {
-                            println!("error getting members: {}", e);
-                            return;
-                        }
-                    };
-                    if members.iter().any(|m| m.user.id == message.author.id) {
-                        to_add = Some(members);
-                        break;
-                    }
-                }
-
-                let to_add = match to_add {
-                    Some(x) => x,
-                    None => {
-                        let _ = message
-                            .reply(
-                                ctx,
-                                "Du musst in einem Sprachkanal sein, um !add zu benutzen",
-                            )
-                            .await;
-                        return;
-                    }
-                };
-
-                let _ = message
-                    .channel_id
-                    .say(
-                        ctx,
-                        format!(
-                            "Füge die folgenden Spieler hinzu: {}",
-                            to_add.iter().map(|m| m.user.name.clone()).join(", ")
-                        ),
-                    )
-                    .await;
-
-                let mut guard = self.lobbies.write().await;
-                let lobby = guard.entry(guild_id).or_insert_with(|| {
-                    println!("Creating new lobby");
-                    Lobby::new(ctx.clone())
-                });
-                let mut messengers = self.messengers.write().await;
-                for member in to_add.into_iter() {
-                    let user_id = member.user.id;
-                    let res = lobby.0.send(LobbyMessage::Join(member.user)).await;
-                    if let Err(tokio::sync::mpsc::error::SendError(LobbyMessage::Join(user))) = res
-                    {
-                        // Lobby is closed, create a new one
-                        println!("Creating new lobby (was closed)");
-                        *lobby = Lobby::new(ctx.clone());
-                        let _ = lobby.0.send(LobbyMessage::Join(user)).await;
-                    }
-
-                    messengers.insert(user_id, lobby.clone());
-                }
+                self.join_all(guild_id, message.author, &ctx).await;
             }
             "leave" | "l" => {
-                let removed = self.messengers.write().await.remove(&message.author.id);
-                if let Some(lobby) = removed {
-                    // if we get an Error, the Lobby is already closed (should never happen), so we can ignore it
-                    let _ = lobby.0.send(LobbyMessage::Leave(message.author)).await;
-                }
+                self.leave(message.author).await;
                 channel_id
                     .say(ctx, "Du hast die Lobby verlassen")
                     .await
                     .unwrap();
             }
             "start" | "s" => {
-                if let Some(lobby) = self.lobbies.read().await.get(&message.guild_id.unwrap()) {
-                    let _ = lobby.0.send(LobbyMessage::Start).await;
-                }
+                self.start(message.guild_id.unwrap()).await;
             }
 
             _ => (),
+        }
+    }
+}
+
+impl Controller {
+    async fn join(&self, guild: GuildId, user: User, ctx: &Context) {
+        let mut guard = self.lobbies.write().await;
+        let lobby = guard.entry(guild).or_insert_with(|| {
+            println!("Creating new lobby");
+            Lobby::new(ctx.clone())
+        });
+        let user_id = user.id;
+        let res = lobby.0.send(LobbyMessage::Join(user)).await;
+        if let Err(tokio::sync::mpsc::error::SendError(LobbyMessage::Join(user))) = res {
+            // Lobby is closed, create a new one
+            println!("Creating new lobby (was closed)");
+            *lobby = Lobby::new(ctx.clone());
+            let _ = lobby.0.send(LobbyMessage::Join(user)).await;
+        }
+
+        self.messengers.write().await.insert(user_id, lobby.clone());
+    }
+    async fn join_all(&self, guild: GuildId, user: User, ctx: &Context) {
+        let channels = match ctx.http.get_channels(guild.0).await {
+            Ok(x) => x,
+            Err(e) => {
+                println!("error getting channel: {}", e);
+                return;
+            }
+        };
+
+        let mut to_add = None;
+        for channel in channels.iter().filter(|c| c.kind == ChannelType::Voice) {
+            let members = match channel.members(ctx).await {
+                Ok(x) => x,
+                Err(e) => {
+                    println!("error getting members: {}", e);
+                    return;
+                }
+            };
+            if members.iter().any(|m| m.user.id == user.id) {
+                to_add = Some(members);
+                break;
+            }
+        }
+
+        let to_add = match to_add {
+            Some(x) => x,
+            None => {
+                // TODO
+                return;
+            }
+        };
+
+        let mut guard = self.lobbies.write().await;
+        let lobby = guard.entry(guild).or_insert_with(|| {
+            println!("Creating new lobby");
+            Lobby::new(ctx.clone())
+        });
+        let mut messengers = self.messengers.write().await;
+        for member in to_add.into_iter() {
+            let user_id = member.user.id;
+            let res = lobby.0.send(LobbyMessage::Join(member.user)).await;
+            if let Err(tokio::sync::mpsc::error::SendError(LobbyMessage::Join(user))) = res {
+                // Lobby is closed, create a new one
+                println!("Creating new lobby (was closed)");
+                *lobby = Lobby::new(ctx.clone());
+                let _ = lobby.0.send(LobbyMessage::Join(user)).await;
+            }
+
+            messengers.insert(user_id, lobby.clone());
+        }
+    }
+    async fn leave(&self, user: User) {
+        let removed = self.messengers.write().await.remove(&user.id);
+        if let Some(lobby) = removed {
+            // if we get an Error, the Lobby is already closed (should never happen), so we can ignore it
+            let _ = lobby.0.send(LobbyMessage::Leave(user)).await;
+        }
+    }
+    async fn start(&self, guild: GuildId) {
+        if let Some(lobby) = self.lobbies.read().await.get(&guild) {
+            let _ = lobby.0.send(LobbyMessage::Start).await;
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 #![warn(clippy::all)]
 
-use serenity::{
-    client::{Client, Context},
-    framework::standard::{
-        macros::{command, group, hook},
-        CommandResult, StandardFramework,
-    },
-    model::channel::Message,
-};
+use serenity::client::Client;
 
 mod controller;
 mod game;
@@ -19,33 +12,13 @@ use controller::Controller;
 
 pub const PREFIX: &str = "!";
 
-#[group]
-#[commands(ping)]
-struct General;
-
-#[hook]
-async fn before(_ctx: &Context, msg: &Message, command_name: &str) -> bool {
-    println!(
-        "Running command '{}' invoked by '{}'",
-        command_name,
-        msg.author.tag()
-    );
-    true
-}
-
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().expect("failed to load .env");
 
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("!"))
-        .before(before)
-        .group(&GENERAL_GROUP);
-
     // Login with a bot token from the environment
     let token = std::env::var("TOKEN").expect("No Token in environment");
     let mut client = Client::builder(token)
-        .framework(framework)
         .event_handler(Controller::new())
         .await
         .expect("Error creating client");
@@ -53,11 +26,4 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
-}
-
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
-
-    Ok(())
 }
